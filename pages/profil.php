e2cea2e397a424153a5968131b5e0811979dbc70
Kanawai-cell <68606422+Kanawai-cell@users.noreply.github.com>
2021-01-24 19:03:44 +0100

    Add files via upload

--- pages/profil.php
+++ pages/profil.php
@@ -7,8 +7,21 @@ Bonjour
 </p>
 
 <!-- à décommenté si ont veut insert quelquechose
- 
 <p>
 Bonjour
-    <?= $post->insertPosts()?>
+    < ?= $post->insertPosts()?>
+</p> -->
+
+
+<!-- à décommenté si ont veut update quelquechose
+<p>
+Bonjour
+    < ?= $post->updatePosts()?>
+</p> -->
+
+
+<!-- à décommenté si ont veut delete quelquechose
+<p>
+Bonjour
+    < ?=$post->deletePosts()?>
 </p> -->
