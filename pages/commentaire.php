f6a721caa47fa2bcad4231430387c3feb2041b12
Kanawai-cell <anisssaghi@gmail.com>
2021-02-16 21:19:22 +0100

    Les commentaires s'affichent et peuvent être insert
    Les commentaires s'affichent et peuvent être insert

--- pages/commentaire.php
+++ pages/commentaire.php
@@ -9,5 +9,25 @@ $commentaire->getCommentaires((int)$_POST['commentaire']);
 
 
 // header("refresh:3;index.php?page=accueil"); // Refresh 3 second and redirect to index.php
-?>
 
+
+print '<form method="post" class="form-horizontal" enctype="multipart/form-data" action="index.php?page=insertCommentaire">';
+
+print '<div class="form-group">';
+print '<label class="col-sm-3 control-label">Commentaire</label>';
+print '<div class="col-sm-6">';
+print '<textarea type="text" name="txt_name" class="form-control" rows="3"></textarea>';
+print '</div>';
+print '</div>';
+
+print '<div class="form-group">';
+print '<div class="col-sm-offset-3 col-sm-9 m-t-15">';
+print '<input name="idpost" type="hidden" value="'.$_POST['commentaire'].'" /> ';
+print '<input type="submit" name="btn_insert" class="btn btn-success" value="Insert">';
+print '<a href="index.php?page=accueil" class="btn btn-danger">Cancel</a>';
+print '</div>';
+print '</div>';
+
+print '</form>';
+
+?>
--- pages/insertCommentaire.php
+++ pages/insertCommentaire.php
@@ -1,21 +1,20 @@
 <?php
+    $name = $_REQUEST['txt_name']; // TextBox name "txt_name"
 
-// if (!isset($errorMsg)) 
-// {
-//     $query = getPdo()->prepare('INSERT INTO post (contenu, publie, date_publication, FK_id_membre, image, name_image) 
-//     VALUES (:contenu, :publie, NOW(), :FK_id_membre, :image, :name_image)');
+    $idPost = (int)$_POST['idpost']; 
 
-//     if ($query->execute([
-//         'contenu' => $name,
-//         'publie' => 1,
-//         'FK_id_membre' => 2,
-//         ':image' => $image_file,
-//         ':name_image' => $name
-//         ])) 
-//     {
-//         $insertMsg = "File Upload Successfully . . . . ."; // Execute query success message
-//         header("refresh:3;index.php?page=accueil"); // Refresh 3 second and redirect to index.php
-//     }
-// }
+    var_dump($_SESSION['pseudo']);
 
-?>
+    var_dump($idPost);
+
+    $query = getPdo()->prepare('INSERT INTO commentaire (id_post, commentaire, auteur, date_commentaire) 
+    VALUES ((SELECT UUID_post FROM post WHERE UUID_post = '.$idPost.'), :commentaire, :auteur, NOW())');
+
+    $query->execute([
+        'commentaire' => $name,
+        'auteur' => $_SESSION['pseudo'] 
+    ]);
+
+    header("refresh:3;index.php?page=accueil"); // Refresh 3 second and redirect to index.php
+
+    ?>
