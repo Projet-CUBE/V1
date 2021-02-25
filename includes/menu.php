8778a15a8d36ebd458f84d2c9aed3e6682a359a3
Kanawai-cell <anisssaghi@gmail.com>
2021-02-25 14:31:22 +0100

    Update menu.php

--- includes/menu.php
+++ includes/menu.php
@@ -6,7 +6,6 @@
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <?php if ($member->isLogged()): ?>
