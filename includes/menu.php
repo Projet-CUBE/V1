60ff1403ebbd90e1a1865dd7ecc9783ac2cb8865
Kanawai-cell <anisssaghi@gmail.com>
2021-02-26 10:40:56 +0100

    Le menu Hamberger WORKS YESSSSSSSSSSSSSSSSSSSSSSSSS !!!!!!!!!!!!!
    Le menu Hamberger WORKS YESSSSSSSSSSSSSSSSSSSSSSSSS !!!!!!!!!!!!! YEAH !!!!!!!!!!!!!!!!

--- includes/menu.php
+++ includes/menu.php
@@ -1,26 +1,21 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light mb-4">
-    <div class="container">
+
+<div class="topnav" id="myTopnav">
         <a class="navbar-brand" href="#">Espace Membres</a>
 
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
                 <?php if ($member->isLogged()): ?>
-                    <li class="nav-item"><a class="nav-link" href="index.php?page=accueil">Accueil</a></li>
-                    <li class="nav-item"><a class="nav-link" href="index.php?page=statistique">Statistique</a></li>
-                    <li class="nav-item"><a class="nav-link" href="index.php?page=gestion-comptes">Gestion des comptes</a></li>
-                    <li class="nav-item"><a class="nav-link" href="index.php?page=evenements">Evènements</a></li>
-                    <li class="nav-item"><a class="nav-link" href="index.php?page=profil">Profil</a></li>
-                    <li class="nav-item"><a class="nav-link" href="index.php?page=deconnexion">Déconnexion</a></li>
+                    <a href="index.php?page=accueil">Accueil</a>
+                    <a href="index.php?page=statistique">Statistique</a>
+                    <a href="index.php?page=gestion-comptes">Gestion des comptes</a>
+                    <a href="index.php?page=evenements">Evènements</a>
+                    <a href="index.php?page=profil">Profil</a>
+                    <a href="index.php?page=deconnexion">Déconnexion</a>
                 <?php else: ?>
-                    <li class="nav-item"><a class="nav-link" href="index.php?page=accueil">Accueil</a></li>
-                    <li class="nav-item"><a class="nav-link" href="index.php?page=connexion">Connexion</a></li>
-                    <li class="nav-item"><a class="nav-link" href="index.php?page=inscription">Inscription</a></li>
+                    <a href="index.php?page=accueil">Accueil</a>
+                    <a href="index.php?page=connexion">Connexion</a>
+                    <a href="index.php?page=inscription">Inscription</a>
                 <?php endif; ?>
-            </ul>
-        </div>
-    </div>
-</nav>
+
+                <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <i class="fa fa-bars"></i>
+  </a>
+</div>             
